b4571998e58e5052d006f5cf19b072bb1c443298
Joseph <nguld@itsj-group.com>
2020-12-01 17:12:53 +0700

    init backend

---
+++ src/main/java/com/example/demo/JosephApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import com.example.demo.entities.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosephApplication implements CommandLineRunner {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  public static void main(String[] args) {
+    SpringApplication.run(JosephApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    userRepository.save(new User("one", "one"));
+    userRepository.save(new User("two", "one"));
+    userRepository.save(new User("three", "one"));
+  }
+}

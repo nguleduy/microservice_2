965a66771bd82035e60f3772c405faf4378e06a1
Joseph <nguld@itsj-group.com>
2020-12-03 10:18:02 +0700

    FE | updated Nid form

--- rest-client/src/app/components/nid-form/nid-form.component.ts
+++ rest-client/src/app/components/nid-form/nid-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Nid} from '../../model/Nid';
+import {UserService} from '../../shared-service/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-nid-form',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NidFormComponent implements OnInit {
 
-  constructor() { }
+  public nid: Nid;
+
+  constructor(private userService: UserService, private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.nid = this.userService.getter();
+  }
+
+  processNidForm() {
+    if (this.nid.id == undefined) {
+      this.userService.createNid(this.nid).subscribe((nid) => {
+        console.log(nid);
+        this.router.navigate(['/']);
+      }, (error) => {
+        console.log(error);
+      });
+    } else {
+      this.userService.updateNid(this.nid).subscribe((nid) => {
+        console.log(nid);
+        this.router.navigate(['/']);
+      }, (error) => {
+        console.log(error);
+      });
+    }
   }
 
 }

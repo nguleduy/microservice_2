a5806a66f55e9f107f02ea830382fe0670a88eaa
Joseph <nguld@itsj-group.com>
2020-12-03 10:37:02 +0700

    FE | nid form validation

--- rest-client/src/app/components/nid-form/nid-form.component.ts
+++ rest-client/src/app/components/nid-form/nid-form.component.ts
@@ -10,13 +10,37 @@ import {Router} from '@angular/router';
 })
 export class NidFormComponent implements OnInit {
 
-  public nid: Nid;
+  public nid = new Nid();
+  public nidError: Nid;
+  public isNidCreated = false;
+  public nidExist = false;
 
   constructor(private userService: UserService, private router: Router) {
   }
 
   ngOnInit(): void {
-    this.nid = this.userService.getter();
+    this.nid = this.userService.getterNid();
+  }
+
+  createNid(): void {
+    this.userService.createNid(this.nid).subscribe(
+      data => {
+        console.log(data);
+        this.nid = new Nid();
+        this.isNidCreated = true;
+        this.nidExist = false;
+        this.nidError = new Nid();
+      },
+      error => {
+        this.nidError = error.error;
+        this.isNidCreated = false;
+        if (error.status === 409) {
+          this.isNidCreated = false;
+          this.nidExist = true;
+        }
+        console.log(error);
+      }
+    );
   }
 
   processNidForm(): void {

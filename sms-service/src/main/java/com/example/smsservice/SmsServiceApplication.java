a50e38d1123ac66faed08be9b38d3b594a6c94bf
Joseph <nguld@itsj-group.com>
2020-12-14 10:39:38 +0700

    BE | clean up

--- sms-service/src/main/java/com/example/smsservice/SmsServiceApplication.java
+++ sms-service/src/main/java/com/example/smsservice/SmsServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class SmsServiceApplication implements CommandLineRunner {
 
   @Autowired
--- user-authentication/src/main/java/com/example/userauthentication/security/jwt/JwtProvider.java
+++ user-authentication/src/main/java/com/example/userauthentication/security/jwt/JwtProvider.java
@@ -20,10 +20,10 @@ public class JwtProvider {
 
   private static final Logger logger = LoggerFactory.getLogger(JwtProvider.class);
 
-  @Value("${grokonez.app.jwtSecret}")
+  @Value("${joseph.app.jwtSecret}")
   private String jwtSecret;
 
-  @Value("${grokonez.app.jwtExpiration}")
+  @Value("${joseph.app.jwtExpiration}")
   private int jwtExpiration;
 
   public String generateJwtToken(Authentication authentication) {

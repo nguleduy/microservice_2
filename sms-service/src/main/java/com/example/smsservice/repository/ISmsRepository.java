4da5250986454809938f2095267e6da795f90201
Joseph <nguld@itsj-group.com>
2020-12-14 10:22:50 +0700

    BE | clean up

--- sms-service/src/main/java/com/example/smsservice/repository/ISmsRepository.java
+++ sms-service/src/main/java/com/example/smsservice/repository/ISmsRepository.java
@@ -4,4 +4,6 @@ import com.example.smsservice.entities.Sms;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ISmsRepository extends JpaRepository<Sms, Long> {
+
+  Sms findById(Integer id);
 }
